2aab0929f65c629ba6a7daa1da5d6721697fc4a3
Ken Are Astrup <ken.astrup@skatteetaten.no>
2024-01-26 14:01:53 +0100

    Byttet ut New-minor-forcing med checkback-stayman

--- generate-pdf.sh
+++ generate-pdf.sh
@@ -1,8 +1,10 @@
 #!/bin/zsh
-asciidoctorpdf \
+asciidoctor-pdf \
   --base-dir src/main/asciidoc \
   --destination-dir out \
   --section-numbers \
-  -a pdf-fontsdir=src/main/resources/fonts \
-  -a pdf-themesdir=src/main/resources/themes \
+  --attribute pdf-fontsdir=src/main/resources/fonts:/Library/Ruby/Gems/2.6.0/gems/asciidoctor-pdf-2.3.11/data/fonts \
+  --attribute pdf-themesdir=src/main/resources/themes \
+  --attribute pdf-theme=bridge \
+  --trace \
   src/main/asciidoc/2-over-1.adoc
