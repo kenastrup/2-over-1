214281b89b8ec4a090cc53bb42cdff776c208187
Ken Are Astrup <ken.astrup@skatteetaten.no>
2024-10-12 23:05:00 +0200

    Virker nå, både PDF og HTML

--- build.gradle.kts
+++ build.gradle.kts
@@ -2,21 +2,24 @@ import org.asciidoctor.gradle.jvm.AsciidoctorTask
 import org.asciidoctor.gradle.jvm.pdf.AsciidoctorPdfTask
 
 plugins {
-    id("org.asciidoctor.jvm.convert") version "3.3.2"
-    id("org.asciidoctor.jvm.pdf") version "3.3.2"
+    id("org.asciidoctor.jvm.convert") version "4.0.3"
+    id("org.asciidoctor.jvm.pdf") version "4.0.3"
+    id("org.asciidoctor.jvm.gems") version "4.0.3"
 }
 
 repositories {
     mavenCentral()
+    ruby.gems()
 }
 val asciidoctorExt: Configuration by configurations.creating
 
 dependencies {
     asciidoctorExt("io.spring.asciidoctor.backends:spring-asciidoctor-backends:0.0.7")
+    asciidoctorGems("rubygems:rouge:3.15.0")
 }
 
 tasks {
-    "asciidoctor"(AsciidoctorTask::class) {
+    named<AsciidoctorTask>("asciidoctor") {
         sourceDir(file("src/main/asciidoc"))
         resources {
             from("src/main/resources/")
@@ -29,9 +32,16 @@ tasks {
         }
         configurations("asciidoctorExt")
     }
-    "asciidoctorPdf"(AsciidoctorPdfTask::class) {
-        sourceDir(file("src/docs/asciidoc"))
-        setFontsDirs(listOf("src/main/resources/fonts"))
+    named<AsciidoctorPdfTask>("asciidoctorPdf") {
+        dependsOn(asciidoctorGemsPrepare)
+        sourceDir(file("src/main/asciidoc"))
+        sources("2-over-1.adoc")
+        setFontsDirs(
+            listOf(
+                "src/main/resources/fonts",
+                //         //"/Library/Ruby/Gems/2.6.0/gems/asciidoctor-pdf-2.3.11/data/fonts"
+            )
+        )
         baseDirFollowsSourceFile()
         setTheme("bridge")
         sources("2-over-1.adoc")
